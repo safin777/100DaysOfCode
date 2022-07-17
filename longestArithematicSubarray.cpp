5eab9dfe782acf3be353d90d2daf8f1d39400d3c
safin777 <sayedislamsafin7770555@gmail.com>
2022-07-17 11:43:48 +0600

    longest Arithematic subarray

---
+++ longestArithematicSubarray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+  int n;
+  cin >> n;
+
+  int a[n];
+  for(int i = 0; i < n; i++){
+    cin >> a[i];
+  }
+
+  int ans =2 ;
+  int pd = a[0] - a[1];
+  int j =2;
+  int curr = 2;
+
+  while (j<n){
+    if (a[j]-a[j-1] == pd){
+      curr++;
+    }
+    else{
+      pd = a[j]-a[j-1];
+      curr = 2;
+    }
+    
+    ans = max(ans, curr);
+    j++;
+  }
+  cout << ans << endl;
+  return 0;
+}
