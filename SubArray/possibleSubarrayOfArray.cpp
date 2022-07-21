a3f2bbfe5fc54194d9878bcf5f808bd3c51206e8
safin777 <sayedislamsafin7770555@gmail.com>
2022-07-21 07:04:17 +0600

    possible sub array from array

---
+++ SubArray/possibleSubarrayOfArray.cpp
@@ -0,0 +1,32 @@
+// take a input array
+// find all the possible subarrays of the array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int n;
+  cin >> n;
+
+  int arr[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+
+  for(int i = 0; i < n; i++)
+  {
+    for(int j = i; j < n; j++)
+    {
+      for(int k = i; k <= j; k++)
+      {
+        cout << arr[k] << " ";
+      }
+      cout << endl;
+      
+    }
+  }
+
+}
